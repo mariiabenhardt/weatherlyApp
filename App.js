083ac38479c3ff522ef5ebc6324744141afaa27a
Dmytro Katrych <zakfaren@gmail.com>
2023-03-30 01:15:35 +0200

    Basic design

--- App.js
+++ App.js
@@ -5,31 +5,45 @@ import { Image } from 'expo-image';
 const weatherIcon = require('./assets/clear_sky.svg')
 
 export default function App() {
+
+  let city = "Your city"
+  let country = "Your country"
+  let temperature = "0"
+  let humidityVal = "0"
+  let windSpeedVal  = "0"
+  let sightDistVal  = "0"
+  let pressureVal = "0"
   return (
     <View style={styles.container}>
 
+          
+        <View style={styles.imageContainer}>
+          <Text style={styles.cityTxt}>{city}</Text>
+          <Text style={styles.countryTxt}>{country}</Text>
+          <Image source={weatherIcon} style={styles.image} />
+          <Text style={styles.tempTxt}>{temperature + " °C"}</Text>
+        </View>
 
-      <View style={styles.imageContainer}>
-       <Text>Your City</Text>
-       <Text>Yout Country</Text>
-        <Image source={weatherIcon} style={styles.image} />
-        <Text>Temperature</Text>
-      </View>
+      
+        <View style={styles.infoBox}>
+          <View
+            style={styles.secondaryInf}>
+            <Text style={styles.secondaryTxt}>Humidity</Text>
+            <Text style={styles.secondaryTxt}>{humidityVal + " %"}</Text>
+            <Text style={styles.secondaryTxt}>Wind</Text>
+            <Text style={styles.secondaryTxt}>{windSpeedVal + " km/h"}</Text>
+          </View>
 
-       <View
-        style={styles.secondaryInf}>
-         <Text style={styles.secondaryTxt}>Humidity</Text>
-         <Text style={styles.secondaryTxt}>Wind</Text>
-      </View>
-
-      <View
-        style={styles.secondaryInf}>
-        <Text style={styles.secondaryTxt}>Sight distance</Text>
-        <Text style={styles.secondaryTxt}>Pressure</Text>
-      </View>
-
+          <View
+            style={styles.secondaryInf}>
+            <Text style={styles.secondaryTxt}>Sight distance</Text>
+            <Text style={styles.secondaryTxt}>{sightDistVal + " km"}</Text>
+            <Text style={styles.secondaryTxt}>Pressure</Text>
+            <Text style={styles.secondaryTxt}>{pressureVal + " mm"}</Text>
+          </View>
+        </View>
     
-
+      
     <StatusBar style="auto" />
     </View>
   );
@@ -37,29 +51,50 @@ export default function App() {
 
 const styles = StyleSheet.create({
   container: {
-    flex: 0,
-    backgroundColor: 'C7E1FF',
+    flex: 1,
+    backgroundColor: '#C7E1FF',
     alignItems: 'center',
     justifyContent: 'center',
   },
   imageContainer: {
-    flex: 0,
-    paddingTop: 100,
+    flex: 1,
+    paddingTop: 20,
+    alignItems: 'center',
+    justifyContent: 'center',
   },
   image: {
     width: 70,
     height: 70,
     borderRadius: 1,
   },
-  secondaryInf: {
+  tempTxt: {
+    fontSize: 50,
+  },
+  cityTxt: {
+    fontSize: 40,
+  },
+  countryTxt: {
+    fontSize: 15,
+  },
+  infoBox:{
+    flex: 2,
     flexDirection: 'row',
     alignItems: 'center',
+    justifyContent: 'center',
+  },
+  secondaryInf: {
+    flex: 4,
+    flexDirection: 'column',
+    alignItems: 'center',
+    //marginHorizontal: 45
   },
   secondaryTxt: {
-    marginRight: 50,
-    marginLeft: 50,
     fontSize: 20,
-  }
+    marginBottom: 10,
+  },
+  mainTxt: {
+
+  },
 });
 
 
